cc8e3a983268147bbf24717b72c5449ce4dd70be
yichen0331 <163812112+yichen0331@users.noreply.github.com>
2024-04-08 23:33:13 +0800

    110153002 3-1
    單位

---
+++ 110153002/110153002/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace _110153002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.txtCM = new System.Windows.Forms.Label();
+            this.txtIn = new System.Windows.Forms.Label();
+            this.txtM = new System.Windows.Forms.Label();
+            this.txtFt = new System.Windows.Forms.Label();
+            this.txtKM = new System.Windows.Forms.Label();
+            this.txtYard = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.Location = new System.Drawing.Point(68, 57);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(75, 23);
+            this.btnAllClear.TabIndex = 0;
+            this.btnAllClear.Text = "清除";
+            this.btnAllClear.UseVisualStyleBackColor = true;
+            // 
+            // txtCM
+            // 
+            this.txtCM.AutoSize = true;
+            this.txtCM.Location = new System.Drawing.Point(65, 114);
+            this.txtCM.Name = "txtCM";
+            this.txtCM.Size = new System.Drawing.Size(44, 18);
+            this.txtCM.TabIndex = 1;
+            this.txtCM.Text = "公分";
+            this.txtCM.Click += new System.EventHandler(this.txtCM_Click);
+            // 
+            // txtIn
+            // 
+            this.txtIn.AutoSize = true;
+            this.txtIn.Location = new System.Drawing.Point(65, 168);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(44, 18);
+            this.txtIn.TabIndex = 1;
+            this.txtIn.Text = "英吋";
+            // 
+            // txtM
+            // 
+            this.txtM.AutoSize = true;
+            this.txtM.Location = new System.Drawing.Point(220, 114);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(44, 18);
+            this.txtM.TabIndex = 1;
+            this.txtM.Text = "公尺";
+            // 
+            // txtFt
+            // 
+            this.txtFt.AutoSize = true;
+            this.txtFt.Location = new System.Drawing.Point(220, 168);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(44, 18);
+            this.txtFt.TabIndex = 1;
+            this.txtFt.Text = "英呎";
+            // 
+            // txtKM
+            // 
+            this.txtKM.AutoSize = true;
+            this.txtKM.Location = new System.Drawing.Point(392, 114);
+            this.txtKM.Name = "txtKM";
+            this.txtKM.Size = new System.Drawing.Size(44, 18);
+            this.txtKM.TabIndex = 1;
+            this.txtKM.Text = "公里";
+            // 
+            // txtYard
+            // 
+            this.txtYard.AutoSize = true;
+            this.txtYard.Location = new System.Drawing.Point(392, 168);
+            this.txtYard.Name = "txtYard";
+            this.txtYard.Size = new System.Drawing.Size(26, 18);
+            this.txtYard.TabIndex = 1;
+            this.txtYard.Text = "碼";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "txtCM";
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(67, 286);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(400, 29);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(222, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 29);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(223, 189);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 29);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(394, 189);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 29);
+            this.textBox5.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(394, 135);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 29);
+            this.textBox6.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(65, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 18);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "說明文字";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(68, 189);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 29);
+            this.textBox7.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.txtYard);
+            this.Controls.Add(this.txtKM);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCM);
+            this.Controls.Add(this.btnAllClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.Label txtCM;
+        private System.Windows.Forms.Label txtIn;
+        private System.Windows.Forms.Label txtM;
+        private System.Windows.Forms.Label txtFt;
+        private System.Windows.Forms.Label txtKM;
+        private System.Windows.Forms.Label txtYard;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ 110153002/110153002/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _110153002
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtCM_Click(object sender, EventArgs e)
+        {
+            double douCM; //宣告一個double變數，變數名稱叫douCM
+
+            douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            //將douCM的數值除以100，也就是從公分轉換成公尺
+        }
+    }
+}
